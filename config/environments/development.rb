79f417447091957e5485d071cecc5425bb5c7240
Masa <cmasad743@gmail.com>
2015-01-01 00:29:31 +0900

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-timetrack::Application.configure do
+Timetrack::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
